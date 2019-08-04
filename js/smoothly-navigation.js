be158093c27c6ebe79d92e222dac0ac7755395fc
ZWW <951598077@qq.com>
2019-08-04 23:51:02 +0800

    优化JS代码

--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,18 +1,22 @@
 !function () {
-    let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+    var view = document.querySelector('nav.menu')
+    var controller = {
+        view: null,
+        aTags: null,
+        init: function (view) {
+            this.view = view
+            this.initAnimation()
+            this.bindEvents()
 
-    function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-
-    for (let i = 0; i < aTags.length; i++) {
-        aTags[i].onclick = function (x) {
-            x.preventDefault()
-            let a = x.currentTarget
-            let href = a.getAttribute('href')
-            let element = document.querySelector(href)
+        },
+        initAnimation: function () {
+            function animate(time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+            requestAnimationFrame(animate);
+        },
+        scrollToElement: function (element) {
             let top = element.offsetTop
             // 上面四句等于下面一句
             // let top = document.querySelector(x.currentTarget.getAttribute('href')).offsetTop
@@ -45,6 +49,20 @@
                     window.scrollTo(0, coords.y)	//如何更新界面
                 })
                 .start();	//开始缓动
+        },
+        bindEvents: function () {
+            let aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+            for (let i = 0; i < aTags.length; i++) {
+                aTags[i].onclick = (x) => {
+                    x.preventDefault()
+                    let a = x.currentTarget
+                    let href = a.getAttribute('href')
+                    let element = document.querySelector(href)
+                    this.scrollToElement(element)
+                }
+            }
         }
     }
+
+    controller.init(view)
 }.call()
